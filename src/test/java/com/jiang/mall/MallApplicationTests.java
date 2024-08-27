04eab73924b13627baf58a21bbca31890beff94d
ab1213home <jiangrongjun2004@163.com>
2024-08-27 12:28:13 +0800

    初始化项目

---
+++ src/test/java/com/jiang/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiang.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
