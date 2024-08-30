76a3c917bbb17cfcfdd8a49441d2fb53e4d8a838
ab1213home <jiangrongjun2004@163.com>
2024-08-30 21:40:17 +0800

    修复优化用户登录注册界面与逻辑，正在进行用户中心系统开发

---
+++ src/main/java/com/jiang/mall/controller/GoodController.java
@@ -0,0 +1,14 @@
+package com.jiang.mall.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GoodController {
+
+    @GetMapping("/good.html")
+    public String getGood(@RequestParam(value = "id", defaultValue = "1") int id) {
+        return "You requested good with ID: " + id;
+    }
+}
