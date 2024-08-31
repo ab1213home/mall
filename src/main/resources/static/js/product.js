63ad42d5763925e4061277d8331b2e1016235c5b
ab1213home <jiangrongjun2004@163.com>
2024-08-31 23:19:43 +0800

    正在进行用户中心系统,商品详情页开发

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+	isLogin();
+})
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const productId = urlParams.get('id');
@@ -16,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.getElementById('productTitle').textContent = product.title;
                 document.getElementById('productImg').src = product.img;
                 document.getElementById('productCode').textContent = product.code;
-                document.getElementById('productCategory').textContent = product.categoryId;
+                document.getElementById('productCategory').textContent = product.categoryName;
                 document.getElementById('productPrice').textContent = product.price;
                 document.getElementById('productStocks').textContent = product.stocks;
                 document.getElementById('productDescription').textContent = product.description;
@@ -25,4 +28,55 @@ document.addEventListener('DOMContentLoaded', function() {
 			}
 		}
 	});
+
+    // 绑定减号按钮点击事件
+    document.getElementById('minus-btn').addEventListener('click', function() {
+        var num = parseInt(document.getElementById('productNum').value);
+        if (num > 1) {
+            num--;
+            document.getElementById('productNum').value = num;
+        }else {
+            alert('购买数量不能小于1');
+        }
+    });
+
+    // 绑加号按钮点击事件
+    document.getElementById('plus-btn').addEventListener('click', function() {
+        var num = parseInt(document.getElementById('productNum').value);
+        if (num<parseInt(document.getElementById('productStocks').textContent)){
+            num++;
+            document.getElementById('productNum').value = num;
+        }else {
+            alert('库存不足，可能无法正常购买，请注意购买数量');
+            num++;
+            document.getElementById('productNum').value = num;
+        }
+
+    });
 });
+function isLogin(){
+	let result = false;
+	$.ajax({
+		type:"GET",
+		url:"/user/isLogin",
+		data:{},
+		async:false,	//设置同步请求
+		dataType:"json",
+		success:function(res){
+			if(res.code == 200){
+				//已登录
+				$("#username").html("<a href='./user/index.html'>"+"你好! " + res.data.username);
+				document.getElementById('register').style.display = 'none';
+				document.getElementById('register_spacer').style.display = 'none';
+				sessionStorage.setItem("userId", res.data.id);
+				result = true;
+			}else{
+				//未登录
+				$("#cartNoLogin").show();
+				$("#cartLogin").hide();
+				result = false;
+			}
+		}
+	});
+	return result;
+}
