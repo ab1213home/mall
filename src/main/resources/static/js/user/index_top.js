693077eb082bf8b84b990d2a18cf4f55f383acce
ab1213home <jiangrongjun2004@163.com>
2024-09-17 16:26:33 +0800

    refactor(user): 移除登出功能并更新modify和login页面布局
    - 从user/index_top.js中移除登出逻辑。- 更新modify子页面(address, info, password)和login.html的navbar下拉菜单样式。
    - 在login.html和register.html中引入bootstrap样式。

---
+++ src/main/resources/static/js/user/index_top.js
@@ -0,0 +1,39 @@
+// 定义登出函数
+function logout() {
+  // 清除登录状态（清除 token 或 session）
+  localStorage.removeItem('token');
+  sessionStorage.removeItem('user');
+  $.ajax({
+		type:"GET",
+		url:"/user/logout",
+		data:{},
+		async:false,	//设置同步请求
+		dataType:"json",
+		success:function(res){
+			if(res.code == 200){
+				// 跳转到登录页面
+				window.location.href = '/user/login.html';
+			}
+		}
+	});
+}
+
+// 获取目标元素
+const dropdownMenu = document.getElementById('dropdown-menu');
+
+// 创建各个元素
+const dropdownItem = document.createElement('a');
+dropdownItem.className = 'dropdown-item';
+dropdownItem.innerHTML = `
+  <i class="mdi mdi-logout text-primary"></i>
+  用户登出
+`;
+
+// 添加点击事件处理程序
+dropdownItem.addEventListener('click', function (event) {
+  event.preventDefault(); // 阻止默认行为（例如跳转）
+  logout(); // 调用登出函数
+});
+
+// 将创建的元素插入到目标元素中
+dropdownMenu.appendChild(dropdownItem);
