f0982eb84dacaa004e03f532449caa852684c22d
ab1213home <jiangrongjun2004@163.com>
2024-09-26 10:42:45 +0800

    ```添加启动脚本以检查和清理现有容器
    更新了项目的run.sh脚本，以包括在构建新的Docker镜像之前检查是否存在名为'mall'的容器，并在必要时停止并删除它。此外，还重新排序了脚本中的DockerCompose命令，以确保正确启动服务。
    ```

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# 检查是否存在名为 mall 的容器，并停止和删除它
+if docker container ls -a | grep -q mall; then
+    docker stop mall
+    docker rm mall
+fi
+
+# 构建 Docker 镜像
 docker build -t mall .
+
+# 启动 Docker Compose 服务
 docker-compose up
