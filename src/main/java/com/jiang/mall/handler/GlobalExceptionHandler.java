3dd275972369b76a2df77e6f7314f3a81c4a0910
ab1213home <jiangrongjun2004@163.com>
2024-11-03 22:08:18 +0800

    feat静态资源: 优化富文本编辑器 Quill 配置和样式
    - 添加自定义样式，调整编辑器高度和字体大小等
    - 引入 Quill Image Uploader 插件，支持图片上传
    - 实现图片上传接口
    - 优化工具栏配置，增加更多编辑功能
    
    Signed-off-by: ab1213home <jiangrongjun2004@163.com>

--- src/main/java/com/jiang/mall/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jiang/mall/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.jiang.mall.handler;
 
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,10 +11,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(@NotNull IllegalArgumentException ex) {
         // 记录异常信息
-        System.err.println("无效请求: " + ex.getMessage());
+	    logger.error("无效请求: {}", ex.getMessage());
         return new ResponseEntity<>("无效请求", HttpStatus.BAD_REQUEST);
     }
 }
