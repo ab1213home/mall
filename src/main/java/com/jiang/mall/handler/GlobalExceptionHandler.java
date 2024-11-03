e0d6e2f72d3030aecd2789219b00daee0cb6a24d
ab1213home <jiangrongjun2004@163.com>
2024-11-03 22:22:34 +0800

    feat: 添加 404错误页面并扩展异常处理功能
    - 新增 404.html 页面，用于处理页面未找到的错误
    - 在 GlobalExceptionHandler 中添加了处理其他异常的代码注释
    - 优化了异常处理逻辑，增加了返回错误信息的方法
    
    Signed-off-by: ab1213home <jiangrongjun2004@163.com>

--- src/main/java/com/jiang/mall/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jiang/mall/handler/GlobalExceptionHandler.java
@@ -19,4 +19,30 @@ public class GlobalExceptionHandler {
 	    logger.error("无效请求: {}", ex.getMessage());
         return new ResponseEntity<>("无效请求", HttpStatus.BAD_REQUEST);
     }
+
+//    // 捕获 PathVariable 参数类型不匹配或格式错误的异常，并返回错误信息
+//    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ResponseBody
+//    public Map<String, Object> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+//        return createErrorResponse(HttpStatus.BAD_REQUEST.value(), "请求参数有误: " + ex.getMessage());
+//    }
+//
+//    // 捕获其他未处理的异常，并返回错误信息
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    @ResponseBody
+//    public Map<String, Object> handleUncaughtException(Exception ex) {
+//        return createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "系统内部错误: " + ex.getMessage());
+//    }
+//
+//    // 创建包含错误码和错误消息的 Map 对象
+//    private Map<String, Object> createErrorResponse(int code, String message) {
+//        Map<String, Object> errorResponse = new HashMap<>();
+//        errorResponse.put("code", code);
+//        errorResponse.put("message", message);
+//        return errorResponse;
+//    }
+
+
 }
