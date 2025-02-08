4046d7726e47b9425235ae4dd67576a3ca776bf5
ab1213home <jiangrongjun2004@163.com>
2025-02-08 16:20:56 +0800

    feat(file): 添加储存配置功能- 新增储存配置页面，支持本地储存和对象储存的配置- 添加储存配置的表单和保存逻辑 - 实现储存配置的编辑功能 - 优化储存配置列表的展示

--- mall-admin/src/main/resources/static/js/admin/file/setting.js
+++ mall-admin/src/main/resources/static/js/admin/file/setting.js
@@ -95,7 +95,10 @@ function getDetailSetting() {
                     row+=`
                     <div class="col-sm-3 mb-3 ${ index == 0?"mb-sm-0":''}">
                         <div class="card ${list.health==true?"text-bg-success":'text-bg-danger'}">
-                            <div class="card-header">${list.name}</div>
+                            <div class="card-header d-flex justify-content-between align-items-center">
+                                <span>${list.name}</span>
+                                <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#storageModal" data-bs-type="edit" data-bs-prod-id="${index}">编辑</button>
+                            </div>
                             <div class="card-body"
                                 <ul>
                                   <li>${list.isDefault?"默认储存":"普通储存"}</li>
@@ -138,3 +141,177 @@ $(document).ready(function () {
     getDetailSetting();
     getFileSize();
 })
+
+function getDetailSettingById(id) {
+    $('#name').val(storageConfig[id].name);
+    $('#isDefault').prop('checked', storageConfig[id].isDefault);
+    const local = document.querySelectorAll('.local');
+    const s3 = document.querySelectorAll('.s3');
+    if (storageConfig[id].type=="local"){
+        $('#type').val("local");
+        $('#path').val(storageConfig[id].path);
+        $('#max-size').val(storageConfig[id].maxSize);
+        local.forEach(function(item) {
+            item.style.display = 'block';
+        });
+        s3.forEach(function(item) {
+            item.style.display = 'none';
+        });
+    }else if (storageConfig[id].type=="s3"){
+        $('#type').val("s3");
+        $('#access-key').val(storageConfig[id].accessKey);
+        $('#secret-key').val(storageConfig[id].secretKey);
+        $('#bucket').val(storageConfig[id].bucket);
+        $('#endpoint').val(storageConfig[id].endpoint);
+        $('#region').val(storageConfig[id].region);
+        local.forEach(function(item) {
+            item.style.display = 'none';
+        });
+        s3.forEach(function(item) {
+            item.style.display = 'block';
+        });
+    }
+}
+
+function insertDetailSetting() {
+    const name = $('#name').val();
+    const type = $('#type').val();
+    const isDefault = $('#isDefault').prop('checked');
+    let data;
+    if (type=="local"){
+        const path = $('#path').val();
+        let maxSize = $('#max-size').val();
+        if (maxSize === '') {
+            maxSize = -1; // 如果为空，默认设置为 -1
+        } else {
+            maxSize = parseFloat(maxSize);
+            if (isNaN(maxSize)) {
+                show_warning('存储最大值必须是有效的数字');
+                return;
+            }
+        }
+        data = {
+            name: name,
+            path: path,
+            maxSize: maxSize,
+            isDefault: isDefault
+        }
+    }else if (type=="s3"){
+        const accessKey = $('#access-key').val();
+        const secretKey = $('#secret-key').val();
+        const bucket = $('#bucket').val();
+        const endpoint = $('#endpoint').val();
+        const region = $('#region').val();
+        data = {
+            name: name,
+            accessKey: accessKey,
+            secretKey: secretKey,
+            bucket: bucket,
+            endpoint: endpoint,
+            region: region,
+            isDefault: isDefault
+        }
+    }
+    data={
+            name: name,
+            type: type,
+            config:data
+            }
+    $.ajax({
+        url: '/file/admin/saveDetailSetting',
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function(res) {
+            if (res.code == 200) {
+                $('#storageModal').modal('hide');
+                show_success("添加储存配置成功");
+                getDetailSetting();
+            }else{
+                show_error("添加储存配置失败："+res.message)
+            }
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var itemModal = document.getElementById('storageModal');
+
+    itemModal.addEventListener("show.bs.modal", function(event) {
+        var button = event.relatedTarget;
+        var type = button.getAttribute('data-bs-type');
+        var modalTitle = document.getElementById('storageModalLabel');
+        var submitBtn = document.getElementById('storageSubmit');
+        $('form').on('submit', function(event) {
+			event.preventDefault(); // 阻止默认提交行为
+            insertDetailSetting();
+			// if (type === 'add') {
+			// 	insertDetailSetting(); // 自定义提交处理
+			// }else if (type === 'edit'){
+			// 	// let id = button.getAttribute('data-bs-prod-id');
+			// 	// updateDetailSetting(id); // 自定义提交处理
+            //     insertDetailSetting();
+			// }
+		});
+        if (type === 'add') {
+            modalTitle.textContent = '添加储存配置';
+            submitBtn.textContent = '添加';
+            clearModal();
+        } else if (type === 'edit') {
+            modalTitle.textContent = '编辑储存配置';
+            submitBtn.textContent = '保存';
+            let id = button.getAttribute('data-bs-prod-id');
+            clearModal();
+            getDetailSettingById(id);
+        }
+    });
+    // 绑定模态框关闭事件
+    itemModal.addEventListener("hidden.bs.modal", function(event) {
+        // 清除表单提交事件
+        $('form').off('submit');
+    });
+});
+
+function clearModal() {
+    $('#name').val('');
+    const local = document.querySelectorAll('.local');
+    local.forEach(element => {
+        element.style.display = 'block';
+    });
+    const s3 = document.querySelectorAll('.s3');
+    s3.forEach(element => {
+        element.style.display = 'none';
+    });
+    $('#type').val('local');
+    $('#path').val('');
+    $('#max-size').val('');
+    $('#secret-key').val('');
+    $('#access-key').val('');
+    $('#bucket').val('');
+    $('#endpoint').val('');
+    $('#region').val('');
+    $('#isDefault').prop('checked', false);
+}
+
+$('#type').on('change', function () {
+    const local = document.querySelectorAll('.local');
+    const s3 = document.querySelectorAll('.s3');
+
+    if ($(this).val() == 'local') {
+        local.forEach(element => {
+            element.style.display = 'block';
+        });
+        s3.forEach(element => {
+            element.style.display = 'none';
+        });
+    } else if ($(this).val() == 's3') {
+        local.forEach(element => {
+            element.style.display = 'none';
+        });
+        s3.forEach(element => {
+            element.style.display = 'block';
+        });
+   }
+
+});
