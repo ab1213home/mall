438aa789af41acb550bf68531c88671e78a9466b
ab1213home <jiangrongjun2004@163.com>
2025-03-22 09:50:31 +0800

    feat(user): 重构权限控制并添加分类管理功能
    - 重构了 AdminInterceptor 类，使用注解实现权限控制
    - 在 CategoryAdminController 中添加了分类设置相关接口- 新增 CategorySettingVo 类用于分类设置
    - 更新 CategoryConfig 类，增加分类设置相关方法
    - 删除了未使用的 EsProduct 相关类和接口
    - 新增 Permission 注解用于权限控制
    - 新增 PermissionType 枚举用于定义权限类型

---
+++ mall-captcha/src/main/java/com/jiang/mall/controller/CaptchaAdminController.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 Jiang RongJun
+ * Jiang Mall is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ * PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package com.jiang.mall.controller;
+
+import com.jiang.mall.annotation.Permission;
+import com.jiang.mall.config.CaptchaConfig;
+import com.jiang.mall.domain.ResponseResult;
+import com.jiang.mall.domain.enums.PermissionType;
+import com.jiang.mall.domain.vo.CaptchaSettingVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * 轮播图控制器
+ * @author jiang
+ * @version 1.0
+ * @since 2024年9月8日
+ */
+@RestController
+@RequestMapping("/captcha/admin")
+public class CaptchaAdminController {
+
+    private CaptchaConfig captchaConfig;
+
+    @Autowired
+    public void setCaptchaConfig(CaptchaConfig captchaConfig) {
+        this.captchaConfig = captchaConfig;
+    }
+
+    @GetMapping("/getSetting")
+    @Permission(type = PermissionType.SYSTEM, value = "system:banner")
+    public ResponseResult<Object> getSetting() {
+        return ResponseResult.okResult(captchaConfig.getCaptchaSetting());
+    }
+
+    @PostMapping("/saveSetting")
+    @Permission(type = PermissionType.SYSTEM, value = "system:banner")
+    public ResponseResult<Object> saveSetting(@RequestBody CaptchaSettingVo captchaSettingVo) {
+        captchaConfig.updateCaptchaSetting(captchaSettingVo);
+        return ResponseResult.okResult();
+    }
+}
