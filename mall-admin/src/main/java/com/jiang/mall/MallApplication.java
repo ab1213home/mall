05c1032f9a2b6e3fb5aa1f6bf6e750da2add2d7f
ab1213home <jiangrongjun2004@163.com>
2025-03-29 15:53:12 +0800

    refactor(mall): 重构项目并添加新功能
    - 移除了 MallApplication 中的 ComponentScan 注解
    - 精简了 MessageServiceImpl 中的方法
    - 新增了 PhoneConfig 和 PhoneConfigItems 类
    - 更新了项目依赖版本:  - Spring Boot: 3.4.1 -> 3.4.3
      - MySQL:9.1.0 -> 9.2.0
      - Logback: 移除具体版本，使用变量引用
      - 新增多个依赖，包括阿里云短信验证码、BouncyCastle 等- 在 pom.xml 中添加了多个新模块依赖

--- mall-admin/src/main/java/com/jiang/mall/MallApplication.java
+++ mall-admin/src/main/java/com/jiang/mall/MallApplication.java
@@ -13,11 +13,8 @@
 
 package com.jiang.mall;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -26,7 +23,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableScheduling
-//@ComponentScan(basePackages = {"com.jiang.mall.config","com.jiang.mall.intercepter","com.jiang.mall.service"})
 public class MallApplication {
 
     public static void main(String[] args) {
