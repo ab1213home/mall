778e579d5cb1417758d33607471cf7d04b24a89a
ab1213home <jiangrongjun2004@163.com>
2025-04-02 22:56:56 +0800

    docs: 更新 OpenAPI/Swagger 配置并添加相关依赖- 新增 OpenApiConfig 类以配置 OpenAPI 信息 - 在多个模块的 pom.xml 中更新 Mulan PSL v2 许可证 URL 为 https - 在 mall-admin 模块中添加 springdoc-openapi-starter-webmvc-ui 依赖 - 新增 SwaggerConfig 类（已注释）

---
+++ mall-admin/src/main/java/com/jiang/mall/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2024 Jiang RongJun
+ * Jiang Mall is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ * PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package com.jiang.mall.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Jiang Mall")
+                        .version("2.0.1")
+                        .description("Jiang Mall's Description of API.")
+		                .termsOfService("https://github.com/ab1213home/mall.git")
+		                .contact(new io.swagger.v3.oas.models.info.Contact()
+				                .name("Jiang Rongjun")
+				                .email("jiangrongjun2004@163.com")
+				                .url("https://github.com/ab1213home"))
+		                .license(new io.swagger.v3.oas.models.info.License()
+				                .name("Mulan PSL v2")
+				                .url("https://license.coscl.org.cn/MulanPSL2"))
+                );
+    }
+}
---
+++ mall-admin/src/main/java/com/jiang/mall/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2024 Jiang RongJun
+ * Jiang Mall is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ * PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package com.jiang.mall.config;
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//import java.util.Collections;
+//
+//@Configuration
+//public class SwaggerConfig {
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiDetails());
+//    }
+//
+//    private ApiInfo apiDetails(){
+//        return new ApiInfo("Jiang Mall",
+//                "Jiang Mall's Description of API.",
+//                "2.0.1",
+//                "https://github.com/ab1213home/mall.git",
+//                new Contact("ab1213home", "https://github.com/ab1213home", "jiangrongjun2004@163.com"),
+//                "Mulan PSL v2",
+//                "http://license.coscl.org.cn/MulanPSL2",
+//                Collections.emptyList());
+//    }
+//}
